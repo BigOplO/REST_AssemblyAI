cb47e418e910337b029567d7968600aa9c5f39a0
ShaomingLi <ll33457791@gmail.com>
2023-06-26 22:38:31 +0900

    final

--- src/App.java
+++ src/App.java
@@ -4,7 +4,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpRequest.BodyPublisher;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse.BodyHandlers;
 
@@ -12,7 +11,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         Transcript transcript = new Transcript();
-        transcript.setAudio_url("/Users/heaven/Desktop/Thirsy.mp4");
+        transcript.setAudio_url("https://github.com/BigOplO/REST_TEST/blob/main/Thirsty.mp4");
         Gson gson = new Gson();
         String jsonRequest = gson.toJson(transcript);
 
@@ -26,6 +25,31 @@ public class App {
         HttpResponse<String> postResopnse = httpClient.send(postRequest, BodyHandlers.ofString());
         System.out.println(postResopnse.body());
 
+        transcript = gson.fromJson(postResopnse.body(), Transcript.class);
+
+        System.out.println(transcript.getstatus());
+
+         HttpRequest getRequest = HttpRequest.newBuilder()
+        .uri(new URI("https://api.assemblyai.com/v2/transcript/" + transcript.getid()))
+        .header("Authorization", "1bbb329fb5b8467fbe2eef577171dc2b")
+        .build();
+
+        
+
+
+        while(true){
+                HttpResponse<String> getResopnse = httpClient.send(getRequest, BodyHandlers.ofString());
+             transcript = gson.fromJson(getResopnse.body(), Transcript.class);
+            
+             System.out.println(transcript.getstatus());
+
+             if("completed".equals(transcript.getstatus())||"error".equals(transcript.getstatus()))
+             break;
+
+             Thread.sleep(1000);
+        }
+
+        System.out.println(transcript.gettext());
 
     }
 }
--- src/Transcript.java
+++ src/Transcript.java
@@ -1,5 +1,8 @@
 public class Transcript {
     private String audio_url;
+    private String id;
+    private String status;
+    private String text;
 
     public String getAudio_url(){
         return audio_url;
@@ -7,5 +10,27 @@ public class Transcript {
 
     public void setAudio_url(String audio_url){
         this.audio_url = audio_url;
+    }
+
+     public String getid(){
+        return id;
+    }
+
+    public void setid(String id){
+        this.id = id;
+    }
+     public String getstatus(){
+        return status;
+    }
+
+    public void setstatus(String status){
+        this.status = status;
+    }
+     public String gettext(){
+        return text;
+    }
+
+    public void settext(String text){
+        this.text = text;
     }
 }
